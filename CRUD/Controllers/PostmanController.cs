35763f7b1355681febf0d9b444fb0d52217bd2a3
Cesar Verne <jonasmauche@gmail.com>
2025-09-28 16:53:12 -0300

    Criação do Controle para Acesso ao Postman
    Criação do Controle para acesso ao Postman

---
+++ CRUD/Controllers/PostmanController.cs
@@ -0,0 +1,37 @@
+﻿using CRUD.Models;
+using CRUD.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CRUD.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PostmanController : ControllerBase
+    {
+        public static readonly PostmanService _postmansService = new PostmanService();
+
+        [HttpGet("")]
+        public async Task<ActionResult> Get()
+        {
+            await Task.Delay(500);
+            return Ok("Expression building program based on lessons! Modulo postman.");
+        }
+
+        [HttpGet("all")]
+        public async Task<string> GetAll()
+        {
+            return await _postmansService.GetAsync();
+        }
+
+        [HttpPut("")]
+        public async Task<IActionResult> UpdateLanguage(AtribuirLigacao ligacao)
+        {
+            long result = 0; 
+            //result = await _postmansService.UpdateLanguageAsync(ligacao.chave.linguagem, ligacao.atualizar.linguagem);
+            string expression = "Expression update " + result + " successfull with.";
+            return Ok(expression);
+        }
+    }
+}
---
+++ CRUD/Services/PostmanService.cs
@@ -0,0 +1,45 @@
+﻿using CRUD.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CRUD.Services
+{
+    public class PostmanService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _postman_api_url = "https://api.getpostman.com/collections";
+        private readonly string _postman_api_key = "PMAK-68d97172c1a8c10001cf7c95-e081e4e950dcada5d69c721fa9ba134875";
+
+        public PostmanService()
+        {
+            HttpClient httpClient = new HttpClient();
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> GetAsync()
+        {
+            _httpClient.DefaultRequestHeaders.Add("X-API-Key", _postman_api_key);
+            var response = await _httpClient.GetAsync(_postman_api_url);
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                return result;
+            }
+            return string.Empty;
+        }
+
+        public async Task<long> UpdateLanguageAsync(string language, string new_language)
+        {
+            /*
+            FilterDefinition<Ligacao> filter = Builders<Ligacao>.Filter.Eq(index => index.linguagem, language);
+            UpdateDefinition<Ligacao> update = Builders<Ligacao>.Update.Set(doc => doc.linguagem, new_language);
+            UpdateResult result = await _conjunctionsCollection.UpdateManyAsync(filter, update);
+            */
+            return 0;
+        }
+    }
+}
